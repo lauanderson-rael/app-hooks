4eebec25246c76ac511673f97c2980563da101fe
lauanderson-rael <lauanderson38@gmail.com>
2025-10-06 17:58:12 -0300

    execicios aula 07 - Texts e Views

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-## Atividade Prática: Hooks
+## Atividades Práticas: RN
+
 Objetivo
 
-Criar um contador simples que utiliza o `useState` para gerenciar o valor e o `useEffect` para reagir a mudanças no estado, fornecendo um feedback visual.
+1 - Criar um contador simples que utiliza o `useState` para gerenciar o valor e o `useEffect` para reagir a mudanças no estado, fornecendo um feedback visual.
+
+2 - Criar um componente de Card com props nome e cargo
+
+3 - Criar um estilizacao hierarquica em um Text
+
+## Imagem do app:
+
+<Img src="image.png" alt="image app"/>
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,40 @@
+import CardProfile from "@/components/CardProfile";
 import Counter from "@/components/Counter";
-import { Text, View } from "react-native";
+import CustomText from "@/components/CustomText";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Contador</Text>
-      <Counter/>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.header}>Exercicio 1 - Contador simples</Text>
+        <Text>Contador</Text>
+        <Counter />
+      </View>
 
+      <View>
+        <Text style={styles.header}>Exercicio 2 - Cartão de Perfil</Text>
+        <CardProfile name="Lauanderson" position="Desenvolvedor FullStack" />
+      </View>
 
+      <View>
+        <Text style={styles.header}>Exercicio 3 - Estilizacao do Text</Text>
+        <CustomText name="IMPORTANTE" />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 20,
+  },
+});
